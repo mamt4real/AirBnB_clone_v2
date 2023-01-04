f0c9aa5091069575a94199bb0b1a402db285f480
= <=>
2023-01-04 11:38:15 +0100

    Task 6 (partly)

--- models/state.py
+++ models/state.py
@@ -1,8 +1,25 @@
 #!/usr/bin/python3
 """ State Module for HBNB project """
-from models.base_model import BaseModel
+from models.base_model import BaseModel, Base
+from sqlalchemy import Column, String
+from sqlalchemy.orm import relationship
+from models.city import City
 
 
-class State(BaseModel):
+class State(BaseModel, Base):
     """ State class """
-    name = ""
+    __tablename__ = "states"
+    name = Column(
+        String(128), nullable=False
+    )
+
+    cities = relationship("City")
+
+    @property
+    def cities(self):
+        """Getter for cities of the state"""
+        from models import storage
+        return list(filter(
+            lambda c: c.state_id == self.id,
+            storage.all(City)
+        ))
