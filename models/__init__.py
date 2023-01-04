24096dc18b02c8250e9ecbf25913707f575b6ba0
mamt4real <mamt4real@gmail.com>
2023-01-04 18:33:18 +0100

    Task 6 Debug II

--- models/__init__.py
+++ models/__init__.py
@@ -1,22 +1,20 @@
 #!/usr/bin/python3
 """This module instantiates an object of class FileStorage"""
-from models.engine.file_storage import FileStorage
-from models.engine.db_storage import DBStorage
-import os
 
+import os
 
 storagetype = os.environ.get("HBNB_TYPE_STORAGE")
 storage = None
 
-
 match storagetype:
     case "file":
+        from models.engine.file_storage import FileStorage
         storage = FileStorage()
     case "db":
+        from models.engine.db_storage import DBStorage
         storage = DBStorage()
     case _ :
         raise ValueError("Storage type is not defined!!")
-
 
 if storage is not None:
     storage.reload()
