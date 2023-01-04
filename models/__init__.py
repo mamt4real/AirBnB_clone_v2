b982d202f4e48b2cdbbdf22613c12ff8f586201c
mamt4real <mamt4real@gmail.com>
2023-01-04 19:04:09 +0100

    Debug II

--- models/__init__.py
+++ models/__init__.py
@@ -7,14 +7,12 @@ storagetype = os.environ.get("HBNB_TYPE_STORAGE")
 storage = None
 
 match storagetype:
-    case "file":
-        from models.engine.file_storage import FileStorage
-        storage = FileStorage()
     case "db":
         from models.engine.db_storage import DBStorage
         storage = DBStorage()
     case _:
-        raise ValueError("Storage type is not defined!!")
+        from models.engine.file_storage import FileStorage
+        storage = FileStorage()
 
 if storage is not None:
     storage.reload()
